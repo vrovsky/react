2d132cbe0248c6716cfea4700c4be653f1cef9d5
Viachaslau Rouski <46630463+vrovsky@users.noreply.github.com>
2020-10-22 00:27:12 +0300

    Update 4-useState-object.js

--- react-advanced-2020-master/src/tutorial/1-useState/setup/4-useState-object.js
+++ react-advanced-2020-master/src/tutorial/1-useState/setup/4-useState-object.js
@@ -3,19 +3,23 @@ import React, { useState } from "react";
 const UseStateObject = () => {
   const [person, setPerson] = useState({
     name: "Slava",
-    age: 24,
+    age: 25,
     message: "random message",
   });
+  const [name, setName] = useState("slava");
+  const [age, setAge] = useState(25);
+  const [message, setMessage] = useState("random message");
 
   const changeMessage = () => {
-    setPerson({ ...person, message: "Hello world" });
+    // setPerson({ ...person, message: "Hello world" });
+    setMessage("hello world");
   };
 
   return (
     <div>
-      <h2>{person.name}</h2>
-      <h3>{person.age}</h3>
-      <h4>{person.message}</h4>
+      <h2>{name}</h2>
+      <h3>{age}</h3>
+      <h4>{message}</h4>
       <button className="btn" onClick={changeMessage}>
         change message
       </button>
