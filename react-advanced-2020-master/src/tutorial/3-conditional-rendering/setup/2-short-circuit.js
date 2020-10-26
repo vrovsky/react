86a393011178d67e66648e74b66709cd30475eab
Viachaslau Rouski <46630463+vrovsky@users.noreply.github.com>
2020-10-26 22:31:48 +0300

    advanced react

--- react-advanced-2020-master/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ react-advanced-2020-master/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  const [text, setText] = useState("");
+  const firstValue = text || "hello world";
+  const secondValue = text && "hello world";
 
-  return <h2>short circuit</h2>;
+  return (
+    <>
+      {/* <h1>{firstValue}</h1> */}
+      {/* <h2>value : {secondValue}</h2> */}
+      <h1>{text || "john doe"}</h1>
+      {!text && <h1>Hello world</h1>}
+    </>
+  );
 };
 
 export default ShortCircuit;
