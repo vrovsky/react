380fdff24ca026741f1edc5ac0d5b7271143aea8
Viachaslau Rouski <46630463+vrovsky@users.noreply.github.com>
2020-11-19 00:11:21 +0300

    cicleLesson

--- rus-react/src/App.js
+++ rus-react/src/App.js
@@ -1,7 +1,18 @@
 import React, { Component } from "react";
+import TodoList from "./Todo/TodoList";
 
 function App() {
-  return <div className="wrapper"></div>;
+  const todos = [
+    { id: 1, completed: false, title: "Купить хлеб" },
+    { id: 2, completed: false, title: "Купить масло" },
+    { id: 3, completed: false, title: "Купить молоко" },
+  ];
+  return (
+    <div className="wrapper">
+      <h1>Tutorial</h1>
+      <TodoList todos={todos} />
+    </div>
+  );
 }
 
 export default App;
